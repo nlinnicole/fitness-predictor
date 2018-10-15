44b75e340ab85bb29cb0a19644d7d0afaaaa6af8
noax <thomasbacks@gmail.com>
2018-10-14 20:35:25 -0400

    Add Weather Apps, so far it works fine, it displays Temperature, wind speed and direction in degree. will need to display it in N, NW, etc... Don't forget to change the path of References, will find a way to set a default path.

---
+++ WeatherApp/src/OwmJapisExample1.java
@@ -0,0 +1,24 @@
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
+
+public class OwmJapisExample1 {
+
+    public static void main(String[] args)
+            throws APIException {
+    	System.out.println("Compiled and runned");
+        // declaring object of "OWM" class
+        OWM owm = new OWM("0eb401a45beb278ea46f2016fb20d3be");
+        owm.setUnit(OWM.Unit.METRIC);
+        // getting current weather data for the "London" city
+        CurrentWeather cwd = owm.currentWeatherByCityName("Montreal,ca");     
+
+        //printing city name from the retrieved data
+        System.out.println("City: " + cwd.getCityName());
+
+        // printing the max./min. temperature
+        System.out.println("Max/Min: " + (cwd.getMainData().getTempMax())
+                            + "/" + (cwd.getMainData().getTempMin()) + "\'C");     
+        System.out.println("Wind: " + cwd.getWindData());
+    }
+}

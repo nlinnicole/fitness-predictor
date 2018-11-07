554ab6a1653c0c4589fcf15993d6a3424f4fef00
Nicole Lin <nlin.nicole@gmail.com>
2018-11-06 23:39:51 -0500

    added ability to change x axis label, dynamically sets y axis max

--- app/src/main/java/com/example/nicole/fitness_predictor/GraphFragment.java
+++ app/src/main/java/com/example/nicole/fitness_predictor/GraphFragment.java
@@ -5,7 +5,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +15,8 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
-
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,21 +28,24 @@ import java.util.ArrayList;
  */
 public class GraphFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "xAxisData";
-    private static final String ARG_PARAM2 = "yAxisData";
-    private static final String ARG_PARAM3 = "title";
-    private static final String ARG_PARAM4 = "yAxisLabel";
+    private static final String ARG_PARAM1 = "xData";
+    private static final String ARG_PARAM2 = "yData";
+    private static final String ARG_PARAM3 = "t";
+    private static final String ARG_PARAM4 = "yLabel";
+    private static final String ARG_PARAM5 = "xLabel";
 
-    //private double[] xAxisData;
-    //private double[] yAxisData;
-    //private String title;
-    //private String yAxis;
+//    private double[] xAxisData;
+//    private double[] yAxisData;
+//    private String title;
+//    private String yAxisLabel;
+//    private String xAxisLabel;
 
     //SAMPLE DATA, delete later
     private double[] xAxisData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     private double[] yAxisData = {14.5, 13.8, 12.9, 14.2, 14.0, 13.2, 13.7, 14.2, 15.2, 14.8};
     private String title = "Average Speed vs. Day";
     private String yAxisLabel = "Average speed";
+    private String xAxisLabel = "Day";
 
     private OnFragmentInteractionListener mListener;
 
@@ -53,20 +57,23 @@ public class GraphFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param xAxisData Parameter 1.
-     * @param yAxisData Parameter 2.
-     * @param title Parameter 3.
-     * @param yAxisLabel Parameter 4
+     * @param xData Parameter 1.
+     * @param yData Parameter 2.
+     * @param t Parameter 3.
+     * @param yLabel Parameter 4
+     * @param xLabel Parameter 5
      * @return A new instance of fragment GraphFragment.
      */
-    public static GraphFragment newInstance(double[] xAxisData, double[] yAxisData, String title, String yAxisLabel) {
+    public static GraphFragment newInstance(double[] xData, double[] yData, String t, String yLabel, String xLabel) {
         GraphFragment fragment = new GraphFragment();
         Bundle args = new Bundle();
-        args.putDoubleArray(ARG_PARAM1, xAxisData);
-        args.putDoubleArray(ARG_PARAM2, yAxisData);
-        args.putString(ARG_PARAM3, title);
-        args.putString(ARG_PARAM4, yAxisLabel);
+        args.putDoubleArray(ARG_PARAM1, xData);
+        args.putDoubleArray(ARG_PARAM2, yData);
+        args.putString(ARG_PARAM3, t);
+        args.putString(ARG_PARAM4, yLabel);
+        args.putString(ARG_PARAM5, xLabel);
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -74,8 +81,12 @@ public class GraphFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            //set instance variables for graph
             xAxisData = getArguments().getDoubleArray(ARG_PARAM1);
             yAxisData = getArguments().getDoubleArray(ARG_PARAM2);
+            title = getArguments().getString(ARG_PARAM3);
+            yAxisLabel = getArguments().getString(ARG_PARAM4);
+            xAxisLabel = getArguments().getString(ARG_PARAM5);
         }
     }
 
@@ -104,12 +115,12 @@ public class GraphFragment extends Fragment {
 
         graph.setTitle(title);
         graph.setTitleTextSize(60);
-        graph.getGridLabelRenderer().setHorizontalAxisTitle("Day");
+        graph.getGridLabelRenderer().setHorizontalAxisTitle(xAxisLabel);
         graph.getGridLabelRenderer().setVerticalAxisTitle(yAxisLabel);
 
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(20);
+        graph.getViewport().setMaxY(getMax()+5);
 
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(1);
@@ -144,6 +155,16 @@ public class GraphFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    private int getMax() {
+        int max = (int) yAxisData[0];
+        for (int i = 0; i < yAxisData.length-1; i++){
+            if (yAxisData[i] > max)
+                max = (int) yAxisData[i];
+        }
+        System.out.println("MAX: "+ max);
+        return max;
     }
 
     /**

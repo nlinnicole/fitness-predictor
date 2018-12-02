66e4e86e6bdc0c25950abae5c667839b1e8eeadd
noax <thomasbacks@gmail.com>
2018-12-02 15:49:04 -0500

    added my test case

--- app/src/androidTest/java/com/example/nicole/fitness_predictor/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nicole/fitness_predictor/ExampleInstrumentedTest.java
@@ -7,6 +7,9 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +25,66 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.nicole.fitness_predictor", appContext.getPackageName());
+    }
+
+    public  void testWindSpeedGraph() {
+        Date currentDate = new Date(118, 9, 25);
+        WeatherFragment weatherFragment = new WeatherFragment();
+        // empty arraylist
+        ArrayList<Date> emptyDates = new ArrayList<>();
+        ArrayList<Double> emptyWindSpeeds = new ArrayList<>();
+
+        // filled arraylist
+        ArrayList<Date> filledDates = new ArrayList<>();
+        ArrayList<Double> filledWindSpeeds = new ArrayList<>();
+        filledDates.add(new Date(currentDate.getYear(), currentDate.getMonth(),currentDate.getDate()));
+        filledWindSpeeds.add(23.2);
+
+        // test the early return.
+        weatherFragment.graphWindSpeed(emptyDates, emptyWindSpeeds);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphWindSpeed(emptyDates, filledWindSpeeds);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphWindSpeed(filledDates, emptyWindSpeeds);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test without the return.
+        weatherFragment.graphWindSpeed(filledDates, filledWindSpeeds);
+        assertEquals(1, weatherFragment.getChildFragmentManager().getFragments().size());
+
+    }
+
+    public  void testAvgTempGraph() {
+        Date currentDate = new Date(118, 9, 25);
+        WeatherFragment weatherFragment = new WeatherFragment();
+        // empty arraylist
+        ArrayList<Date> emptyDates = new ArrayList<>();
+        ArrayList<Double> emptyAvgTemps = new ArrayList<>();
+
+        // filled arraylist
+        ArrayList<Date> filledDates = new ArrayList<>();
+        ArrayList<Double> filledAvgTemps = new ArrayList<>();
+        filledDates.add(new Date(currentDate.getYear(), currentDate.getMonth(),currentDate.getDate()));
+        filledAvgTemps.add(12.0);
+
+        // test the early return.
+        weatherFragment.graphAvgTemp(emptyDates, emptyAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphAvgTemp(emptyDates, filledAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphAvgTemp(filledDates, emptyAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test without the return.
+        weatherFragment.graphAvgTemp(filledDates, filledAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
     }
 }
--- app/src/main/java/com/example/nicole/fitness_predictor/WeatherFragment.java
+++ app/src/main/java/com/example/nicole/fitness_predictor/WeatherFragment.java
@@ -135,7 +135,7 @@ public class WeatherFragment extends Fragment implements GraphFragment.OnFragmen
 
     }
 
-    private void graphWindSpeed(ArrayList<Date> dates, ArrayList<Double> windSpeeds) {
+    public void graphWindSpeed(ArrayList<Date> dates, ArrayList<Double> windSpeeds) {
         if (dates.isEmpty() || windSpeeds.isEmpty()) {
             return;
         }
@@ -156,7 +156,7 @@ public class WeatherFragment extends Fragment implements GraphFragment.OnFragmen
         ft.add(R.id.graphWeatherContainer, graphFragment).commit();
     }
 
-    private void graphAvgTemp(ArrayList<Date> dates, ArrayList<Double> avgTemps) {
+    public void graphAvgTemp(ArrayList<Date> dates, ArrayList<Double> avgTemps) {
         if (dates.isEmpty() || avgTemps.isEmpty()) {
             return;
         }

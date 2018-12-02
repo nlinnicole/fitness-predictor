2990db12270318616a2a4f3d4d4d1e79570cf3e2
Tom Lebreux <tomlebreux@cock.li>
2018-12-02 10:36:37 -0500

    Implement attemptLoginWithKeystore unit tests

--- app/src/androidTest/java/com/example/nicole/fitness_predictor/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nicole/fitness_predictor/ExampleInstrumentedTest.java
@@ -16,11 +16,47 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    /**
+     *  TODO: Should probably be in configuration file / environment variable AND from a dummy account
+     */
+    public static final String TEST_ENDOMONDO_USERNAME = "comp354_2018@yahoo.com";
+    public static final String TEST_ENDOMONDO_PASSWORD = "Concordia";
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.nicole.fitness_predictor", appContext.getPackageName());
+    }
+
+    @Test
+    public void testAttemptLogin() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        LoginController controller = LoginController.getInstance(context);
+
+        /* Test empty username */
+        controller.saveCredentials("", "Password");
+        assertNull(controller.attemptLoginWithKeystore());
+
+        /* Test null username */
+        controller.saveCredentials(null, "Password");
+        assertNull(controller.attemptLoginWithKeystore());
+
+        /* Test empty password */
+        controller.saveCredentials("Username", "");
+        assertNull(controller.attemptLoginWithKeystore());
+
+        /* Test null password */
+        controller.saveCredentials("Username", null);
+        assertNull(controller.attemptLoginWithKeystore());
+
+        /* Test wrong credential */
+        controller.saveCredentials("Username", "Password");
+        assertNull(controller.attemptLoginWithKeystore());
+
+        /* Test valid credential */
+        controller.saveCredentials(TEST_ENDOMONDO_USERNAME, TEST_ENDOMONDO_PASSWORD);
+        assertNotNull(controller.attemptLoginWithKeystore());
     }
 }

516d32e5bf665ba01c2f76c92e1871a12f386188
noax <thomasbacks@gmail.com>
2018-12-04 20:31:15 -0500

    changed the test function to right java file

--- app/src/test/java/com/example/nicole/fitness_predictor/ExampleUnitTest.java
+++ app/src/test/java/com/example/nicole/fitness_predictor/ExampleUnitTest.java
@@ -14,6 +14,65 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    public  void testWindSpeedGraph() {
+        Date currentDate = new Date(118, 9, 25);
+        WeatherFragment weatherFragment = new WeatherFragment();
+        // empty arraylist
+        ArrayList<Date> emptyDates = new ArrayList<>();
+        ArrayList<Double> emptyWindSpeeds = new ArrayList<>();
 
+        // filled arraylist
+        ArrayList<Date> filledDates = new ArrayList<>();
+        ArrayList<Double> filledWindSpeeds = new ArrayList<>();
+        filledDates.add(new Date(currentDate.getYear(), currentDate.getMonth(),currentDate.getDate()));
+        filledWindSpeeds.add(23.2);
+
+        // test the early return.
+        weatherFragment.graphWindSpeed(emptyDates, emptyWindSpeeds);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphWindSpeed(emptyDates, filledWindSpeeds);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphWindSpeed(filledDates, emptyWindSpeeds);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test without the return.
+        weatherFragment.graphWindSpeed(filledDates, filledWindSpeeds);
+        assertEquals(1, weatherFragment.getChildFragmentManager().getFragments().size());
+
+    }
+
+    public  void testAvgTempGraph() {
+        Date currentDate = new Date(118, 9, 25);
+        WeatherFragment weatherFragment = new WeatherFragment();
+        // empty arraylist
+        ArrayList<Date> emptyDates = new ArrayList<>();
+        ArrayList<Double> emptyAvgTemps = new ArrayList<>();
+
+        // filled arraylist
+        ArrayList<Date> filledDates = new ArrayList<>();
+        ArrayList<Double> filledAvgTemps = new ArrayList<>();
+        filledDates.add(new Date(currentDate.getYear(), currentDate.getMonth(),currentDate.getDate()));
+        filledAvgTemps.add(12.0);
+
+        // test the early return.
+        weatherFragment.graphAvgTemp(emptyDates, emptyAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphAvgTemp(emptyDates, filledAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test the early return.
+        weatherFragment.graphAvgTemp(filledDates, emptyAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+
+        // test without the return.
+        weatherFragment.graphAvgTemp(filledDates, filledAvgTemps);
+        assertEquals(0, weatherFragment.getChildFragmentManager().getFragments().size());
+    }
 
 }
